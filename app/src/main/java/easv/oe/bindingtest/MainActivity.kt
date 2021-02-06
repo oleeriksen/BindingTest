bc7939262a3b78e62957b71f1eb9d513e64929a3
oleeriksen <oe@easv.dk>
2021-02-06 23:08:48 +0100

    div

--- app/src/main/java/easv/oe/bindingtest/MainActivity.kt
+++ app/src/main/java/easv/oe/bindingtest/MainActivity.kt
@@ -2,6 +2,7 @@ package easv.oe.bindingtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import easv.oe.bindingtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.tvHeader.text = "ole"
+
+        //findViewById<TextView>(R.id.tvHeader).text = "peter"
         //setContentView(R.layout.activity_main)
 
     }
