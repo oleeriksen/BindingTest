eeae381ce88d121a45c20fd014db913ed8ded8f1
oleeriksen <oe@easv.dk>
2021-02-06 23:26:25 +0100

    div

--- app/src/main/java/easv/oe/bindingtest/MainActivity.kt
+++ app/src/main/java/easv/oe/bindingtest/MainActivity.kt
@@ -9,12 +9,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val mTexts = Texts()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvHeader.text = "ole"
+        binding.tvHeader.text = mTexts.get(1)
 
         //findViewById<TextView>(R.id.tvHeader).text = "peter"
         //setContentView(R.layout.activity_main)
---
+++ app/src/main/java/easv/oe/bindingtest/Texts.java
@@ -0,0 +1,9 @@
+package easv.oe.bindingtest;
+
+public class Texts {
+
+    String[] t = {"peter", "hans"};
+
+    public String get(int idx) { return t[idx]; }
+
+}
